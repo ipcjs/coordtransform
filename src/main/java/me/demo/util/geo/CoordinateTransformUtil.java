51ab4ee043064e047b979ac6d862a42c125127c2
ipcjs <gipcjs@gmail.com>
2017-09-29 20:04:06 +0800

    增强国内外判断的方法

--- src/main/java/me/demo/util/geo/CoordinateTransformUtil.java
+++ src/main/java/me/demo/util/geo/CoordinateTransformUtil.java
@@ -9,6 +9,26 @@ package me.demo.util.geo;
  */
 public class CoordinateTransformUtil {
 
+    //China region - raw data
+    public static final Rectangle[] REGION = {
+            new Rectangle(79.446200, 49.220400, 96.330000, 42.889900),
+            new Rectangle(109.687200, 54.141500, 135.000200, 39.374200),
+            new Rectangle(73.124600, 42.889900, 124.143255, 29.529700),
+            new Rectangle(82.968400, 29.529700, 97.035200, 26.718600),
+            new Rectangle(97.025300, 29.529700, 124.367395, 20.414096),
+            new Rectangle(107.975793, 20.414096, 111.744104, 17.871542)
+    };
+
+    //China excluded region - raw data
+    public static final Rectangle[] EXCLUDE = {
+            new Rectangle(119.921265, 25.398623, 122.497559, 21.785006),
+            new Rectangle(101.865200, 22.284000, 106.665000, 20.098800),
+            new Rectangle(106.452500, 21.542200, 108.051000, 20.487800),
+            new Rectangle(109.032300, 55.817500, 119.127000, 50.325700),
+            new Rectangle(127.456800, 55.817500, 137.022700, 49.557400),
+            new Rectangle(131.266200, 44.892200, 137.022700, 42.569200)
+    };
+
     private static double X_PI = 3.14159265358979324 * 3000.0 / 180.0;
 
     // π
@@ -20,30 +40,47 @@ public class CoordinateTransformUtil {
     // 扁率
     private static double EE = 0.00669342162296594323;
 
-    /**
-     * 百度坐标系(BD-09)转WGS坐标
-     *
-     * @param lng 百度坐标纬度
-     * @param lat 百度坐标经度
-     * @return WGS84坐标数组
-     */
-    public static double[] bd09ToWgs84(double lng, double lat) {
-        double[] gcj = bd09ToGcj02(lng, lat);
-        double[] wgs84 = gcj02ToWgs84(gcj[0], gcj[1]);
-        return wgs84;
+    private static boolean isInChina(double lng, double lat) {
+        for (Rectangle region : REGION) {
+            if (region.contain(lng, lat)) {
+                for (Rectangle exclude : EXCLUDE) {
+                    if (exclude.contain(lng, lat)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
-     * WGS坐标转百度坐标系(BD-09)
+     * WGS84转GCJ02(火星坐标系)
      *
      * @param lng WGS84坐标系的经度
      * @param lat WGS84坐标系的纬度
-     * @return 百度坐标数组
+     * @return 火星坐标数组
      */
-    public static double[] wgs84ToBd09(double lng, double lat) {
-        double[] gcj = wgs84ToGcj02(lng, lat);
-        double[] bd09 = gcj02ToBd09(gcj[0], gcj[1]);
-        return bd09;
+    public static double[] wgs84ToGcj02(double lng, double lat) {
+        if (!isInChina(lng, lat)) {
+            return new double[]{lng, lat};
+        }
+        return transform(lng, lat);
+    }
+
+    /**
+     * GCJ02(火星坐标系)转WGS84
+     *
+     * @param lng 火星坐标系的经度
+     * @param lat 火星坐标系纬度
+     * @return WGS84坐标数组
+     */
+    public static double[] gcj02ToWgs84(double lng, double lat) {
+        if (!isInChina(lng, lat)) {
+            return new double[]{lng, lat};
+        }
+        double[] out = transform(lng, lat);
+        return new double[]{lng * 2 - out[0], lat * 2 - out[1]};
     }
 
     /**
@@ -81,32 +118,29 @@ public class CoordinateTransformUtil {
     }
 
     /**
-     * WGS84转GCJ02(火星坐标系)
+     * WGS坐标转百度坐标系(BD-09)
      *
      * @param lng WGS84坐标系的经度
      * @param lat WGS84坐标系的纬度
-     * @return 火星坐标数组
+     * @return 百度坐标数组
      */
-    public static double[] wgs84ToGcj02(double lng, double lat) {
-        if (outOfChina(lng, lat)) {
-            return new double[]{lng, lat};
-        }
-        return transform(lng, lat);
+    public static double[] wgs84ToBd09(double lng, double lat) {
+        double[] gcj = wgs84ToGcj02(lng, lat);
+        double[] bd09 = gcj02ToBd09(gcj[0], gcj[1]);
+        return bd09;
     }
 
     /**
-     * GCJ02(火星坐标系)转WGS84
+     * 百度坐标系(BD-09)转WGS坐标
      *
-     * @param lng 火星坐标系的经度
-     * @param lat 火星坐标系纬度
+     * @param lng 百度坐标纬度
+     * @param lat 百度坐标经度
      * @return WGS84坐标数组
      */
-    public static double[] gcj02ToWgs84(double lng, double lat) {
-        if (outOfChina(lng, lat)) {
-            return new double[]{lng, lat};
-        }
-        double[] out = transform(lng, lat);
-        return new double[]{lng * 2 - out[0], lat * 2 - out[1]};
+    public static double[] bd09ToWgs84(double lng, double lat) {
+        double[] gcj = bd09ToGcj02(lng, lat);
+        double[] wgs84 = gcj02ToWgs84(gcj[0], gcj[1]);
+        return wgs84;
     }
 
     private static double[] transform(final double lng, final double lat) {
@@ -145,15 +179,25 @@ public class CoordinateTransformUtil {
         return ret;
     }
 
-    /**
-     * 判断是否在国内，不在国内不做偏移
-     */
-    private static boolean outOfChina(double lng, double lat) {
-        if (lng < 72.004 || lng > 137.8347) {
-            return true;
-        } else if (lat < 0.8293 || lat > 55.8271) {
-            return true;
+    private static class Rectangle {
+
+        public final double west;
+
+        public final double north;
+
+        public final double east;
+
+        public final double south;
+
+        public Rectangle(double lng1, double lat1, double lng2, double lat2) {
+            this.west = Math.min(lng1, lng2);
+            this.north = Math.max(lat1, lat2);
+            this.east = Math.max(lng1, lng2);
+            this.south = Math.min(lat1, lat2);
         }
-        return false;
+
+        public boolean contain(double lng, double lat) {
+            return this.west <= lng && this.east >= lng && this.north >= lat && this.south <= lat;
+        }
     }
 }
